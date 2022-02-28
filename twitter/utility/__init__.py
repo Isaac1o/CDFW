9664096cb344bc9fae22ab65e5a3f33bd72c742b
Isaac Lo <isaac.lo9911@gmail.com>
2022-02-28 14:57:20 -0800

    copied Tweepys pagination.py but adjusted it to return user and place data

--- twitter/utility/__init__.py
+++ twitter/utility/__init__.py
@@ -2,3 +2,7 @@ from .text_normalizer import *
 from .get_twitter import *
 from .classify_tweets import *
 from .sentiment import *
+from get_twitter_research import *
+from contractions import *
+from pagination_new import *
+from save_query_to_cvs import *
--- twitter/utility/get_twitter_research.py
+++ twitter/utility/get_twitter_research.py
@@ -1,9 +1,12 @@
-import get_twitter as gt
+import utility.get_twitter as gt
+import utility.pagination_new as new_page
 import tweepy
 import json
+from math import inf
+import time
 
 
-def authenticate_research(research_auth_filename):
+def authenticate_research(research_auth_filename, wait_on_rate_limit=False):
     """
     Return twitter Client object for research license
     """
@@ -13,8 +16,8 @@ def authenticate_research(research_auth_filename):
                            consumer_secret=consumer_secret,
                            access_token=access_token,
                            access_token_secret=access_token_secret,
-                           # return_type=dict,         # Returns python dictionary instead of response object
-                           wait_on_rate_limit=True)  # Whether to wait when rate limit is reached
+                           return_type='response',         # Returns python dictionary instead of response object
+                           wait_on_rate_limit=wait_on_rate_limit)  # Whether to wait when rate limit is reached
     return client
 
 
@@ -23,128 +26,98 @@ def query_to_json(data, file_path):
         json.dump(data, f)
 
 
-def format_response(response, existing_dict=None):
+def coyote_query_string(coords:list) -> str:
+    # Contructs the query that searches a geo-polygon.
+    # The geopolygon is queried using "bounding_box:[west_long south_lat east_long north_lat]"
+    # `coords` is a list in the form [west_long, south_lat, east_long, north_lat]
+    query = f'(coyotes OR coyote -is:retweet -is:reply has:geo lang:en) (bounding_box:[{coords[0]} {coords[1]} {coords[2]} {coords[3]}])'
+    return query
+
+
+def format_response(pages):
     """
     Formats response object into a dictionary with 3 keys: data, user, places.
     - data is a dictionary with {tweet_id: tweet_data}
     - users is a dictionary with {user_id: user_data}
     - places is a dictionary with {place_id: place_data}
 
-    If existing_dict is not None, then append response to the corresponding keys of exisiting_dict
-    :param existing_dict: Dictionary containing previous pages of tweet data
-    :param response: response object returned from twitter api
     :return:
     """
-    if existing_dict:
-        for data in response.data:
-            id = str(data['id'])
-            existing_dict['data'][id] = data.data
-        for user in response.includes['users']:
-            user_id = str(user['id'])
-            existing_dict['users'][user_id] = user.data
-        for place in response.includes['places']:
-            place_id = str(place['id'])
-            existing_dict['places'][place_id] = place.data
+    results = []
+    for p in pages:
+        results.append(p)
+    # print(results)
 
-        return existing_dict
+    if len(results) < 3:
+        return None
+
+    tweet_info = results[:-2]
+    user_info = results[-2]
+    place_info = results[-1]
+    # print(user_info)
 
     info = {
-        'data': {str(data['id']): data.data for data in response.data},
-        'users': {str(user['id']): user.data for user in response.includes['users']},
-        'places': {str(place['id']): place.data for place in response.includes['places']}
+        'data': {str(tweet.data['id']): tweet.data for tweet in tweet_info},
+        'users': {str(user.data['id']): user.data for user in user_info},
+        'places': {str(place.data['id']): place.data for place in place_info}
     }
 
     return info
 
 
-def search_twitter(client, query, next_token=None, existing_data=None, start_time=None, end_time=None, max_results=500):
+def search_twitter(client, query, start_time=None, end_time=None, limit=inf):
     """
     Queries one page and returns the next token and data in a dictionary.
-    :param existing_data:
-    :param next_token: Token for next page
+    :param limit: Total number of tweets to return
     :param client:
     :param query:
     :param start_time:
     :param end_time:
-    :param max_results
     :return: data, next_token, num_tweets
     """
     place_fields = ['full_name', 'country_code', 'geo', 'contained_within', 'country', 'name', 'place_type']
     tweet_fields = ['created_at', 'attachments', 'author_id', 'conversation_id', 'geo', 'lang', 'possibly_sensitive', 'public_metrics']
     user_fields = ['created_at', 'description', 'entities', 'location', 'public_metrics', 'protected', 'profile_image_url', 'pinned_tweet_id', 'verified']
-    response = client.search_all_tweets(
-        query,
+    time.sleep(1)
+    p = new_page.Paginator2(
+        client.search_all_tweets,
+        query=query,
         tweet_fields=tweet_fields,
         expansions=['geo.place_id', 'author_id'],
         place_fields=place_fields,
         user_fields=user_fields,
-        max_results=max_results,
+        max_results=500,
         start_time=start_time,
-        end_time=end_time,
-        next_token=next_token
-    )
-    data = format_response(response, existing_dict=existing_data)
+        end_time=end_time
+    ).flatten(100)
 
-    return data, response.meta['next_token'], response.meta['result_count']
-
-
-def paginator(client, query, start_time=None, end_time=None, num_tweets=100, file_path=None):
-    """
-    Returns dictionary with num_tweets number of tweets
-    :param file_path:
-    :param client:
-    :param query:
-    :param start_time:
-    :param end_time:
-    :param num_tweets:
-    :return:
-    """
-    max_results = 500
-    count = 0
-    next_token = None
-    data = None
-    if num_tweets < max_results:
-        max_results = num_tweets
-    while count < num_tweets:
-        data, next_token, result_count = search_twitter(
-            client,
-            query,
-            next_token=next_token,
-            existing_data=data,
-            start_time=start_time,
-            end_time=end_time,
-            max_results=max_results
-        )
-        count += result_count
-
-    if file_path:
-        query_to_json(data, file_path)
+    data = format_response(p)
 
     return data
 
 
+def paginator(client, geopolygon, start_time=None, end_time=None, max_tweets=100, file_path=None):
+    """
+    Returns dictionary with num_tweets number of tweets
+    :param max_tweets:
+    :param geopolygon: list with coordinates: [west_long south_lat east_long north_lat]
+    :param file_path:
+    :param client:
+    :param start_time:
+    :param end_time:
+    :return:
+    """
+    query = coyote_query_string(geopolygon)
 
+    data = search_twitter(
+        client,
+        query,
+        start_time=start_time,
+        end_time=end_time,
+        limit=max_tweets
+    )
 
-# client = authenticate_research('/Users/Isaacbolo/Licenses/twitter_research.csv')
-# d, tk, ct = search_twitter(client, 'dog has:geo', max_results=10)
-# print(type(d))
-# c, tk2, ct2 = search_twitter(client, 'cat has:geo', max_results=10, existing_data=d)
-# print(c)
-# query_to_json(d, '/tmp/test_json.json')
+    if file_path and data is not None:
+        query_to_json(data, file_path)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return data
